71ae127532ef47d7e2ef99ce95e75bd034dabccf
Colin McNeil <colin.mcneil@docker.com>
2024-07-29 22:50:01 -0400

    Add all necessary prerun

--- src/extension/ui/src/args.ts
+++ src/extension/ui/src/args.ts
@@ -12,13 +12,12 @@ export const getRunArgs = (prompt_ref: string, project_dir: string, username: st
     //                       "github:docker/labs-githooks?ref=main&path=prompts/git_hooks"
     return [
         '--rm',
-        '-it',
         '-v',
         '/var/run/docker.sock:/var/run/docker.sock',
         '--mount',
         'type=volume,source=docker-prompts,target=/prompts',
         '--mount',
-        'type=bind,source=$HOME/.openai-api-key,target=/root/.openai-api-key',
+        'type=bind,source=/root/.openai-api-key,target=/root/.openai-api-key',
         'vonwig/prompts:latest',
         'run',
         project_dir,
