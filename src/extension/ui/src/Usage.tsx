a07a8828ca5f2cc65c33b92f834d650948ad304c
colinmcneil <colin.mcneil@docker.com>
2025-01-29 10:47:09 -0500

    Add claude config change event

--- src/extension/ui/src/Usage.tsx
+++ src/extension/ui/src/Usage.tsx
@@ -2,21 +2,40 @@
  * Anonymous tracking event for registry changes
  */
 
-type Record = {
+interface Record {
     event: string;
     properties: object;
     event_timestamp: number;
     source: string;
 };
 
+export interface RegistryChangedRecord extends Record {
+    event: 'registry-changed';
+    properties: {
+        name: string;
+        ref: string;
+        action: 'remove' | 'add';
+    };
+};
+
+export interface ClaudeConfigChangedRecord extends Record {
+    event: 'claude-config-changed';
+    properties: {
+        action: 'add' | 'remove';
+    };
+};
+
 const eventsQueue: Record[] = [];
 
 let processInterval: NodeJS.Timeout;
 
-export const registryChanged = (name: string, ref: string, action: 'remove' | 'add') => {
+type Event = (RegistryChangedRecord | ClaudeConfigChangedRecord)['event'];
+type Properties = (RegistryChangedRecord | ClaudeConfigChangedRecord)['properties'];
+
+export const trackEvent = (event: Event, properties: Properties) => {
     const record: Record = {
-        event: 'registry-changed',
-        properties: { name, ref, action },
+        event,
+        properties,
         event_timestamp: Date.now(),
         source: 'labs-ai-tools-for-devs-dd'
     };
@@ -38,7 +57,7 @@ const processEventsQueue = () => {
     sendRecords(events);
 };
 
-const sendRecords = (records: any[]) => {
+const sendRecords = (records: Record[]) => {
     const url = 'https://nd14xwptgj.execute-api.us-east-1.amazonaws.com/stage/v1/track';
     const apiKey = '1234567890';
 
