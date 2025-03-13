7d7f7d39e729135cbc2457398de13eb52684e5be
colinmcneil <colin.mcneil@docker.com>
2025-03-12 21:30:50 -0400

    Re-add missing props

--- src/extension/ui/src/components/tabs/YourEnvironment.tsx
+++ src/extension/ui/src/components/tabs/YourEnvironment.tsx
@@ -6,9 +6,10 @@ import { DD_BUILD_WITH_SECRET_SUPPORT, getUnsupportedSecretMessage } from '../..
 interface YourEnvironmentProps {
     secrets: Secrets.Secret[];
     ddVersion: { version: string, build: number };
+    config: { [key: string]: { [key: string]: any } };
 }
 
-const YourEnvironment: React.FC<YourEnvironmentProps> = ({ secrets, ddVersion }) => {
+const YourEnvironment: React.FC<YourEnvironmentProps> = ({ secrets, ddVersion, config }) => {
     const hasDDVersionWithSecretSupport = ddVersion && ddVersion.build >= DD_BUILD_WITH_SECRET_SUPPORT;
 
     if (!hasDDVersionWithSecretSupport) {
@@ -16,22 +17,26 @@ const YourEnvironment: React.FC<YourEnvironmentProps> = ({ secrets, ddVersion })
     }
 
     return (
-        <List subheader={
-            <Stack direction="column" spacing={2} alignItems="center">
-                <Alert title="Docker Secret Management" severity="info" sx={{ fontSize: '1.2rem', maxWidth: 600 }}>
-                    Docker Secret Management is a new feature in Docker Desktop that allows you to securely inject secrets into your containers.
-                </Alert>
-                <Typography variant="h2">
-                    The following secrets are available to use in your prompts:
-                </Typography>
-            </Stack>
-        }>
-            {secrets.map((secret) => (
-                <ListItem key={secret.name}>
-                    <ListItemText primary={<Typography variant="h6">{secret.name}</Typography>} />
-                </ListItem>
-            ))}
-        </List>
+        <Stack direction="column" spacing={2}>
+            <List subheader={
+                <Stack direction="column" spacing={2} alignItems="center">
+                    <Alert title="Docker Secret Management" severity="info" sx={{ fontSize: '1.2rem', maxWidth: 600 }}>
+                        Docker Secret Management is a new feature in Docker Desktop that allows you to securely inject secrets into your containers.
+                    </Alert>
+                    <Typography variant="h2">
+                        The following secrets are available to use in your prompts:
+                    </Typography>
+                </Stack>
+            }>
+                {secrets.map((secret) => (
+                    <ListItem key={secret.name}>
+                        <ListItemText primary={<Typography variant="h6">{secret.name}</Typography>} />
+                    </ListItem>
+                ))}
+            </List>
+            <Typography variant="h2">The following configurations are available to use in your prompts:</Typography>
+            <pre>{JSON.stringify(config, null, 2)}</pre>
+        </Stack>
     );
 };
 
