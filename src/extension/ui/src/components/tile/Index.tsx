a68954774f9edb1b3b09f86fed90f929b688fecf
Trung Nguyen <trung.nguyen@docker.com>
2025-04-24 11:35:27 +0200

    Remove unused Dialog in Tile
    Signed-off-by: Trung Nguyen <trung.nguyen@docker.com>

--- src/extension/ui/src/components/tile/Index.tsx
+++ src/extension/ui/src/components/tile/Index.tsx
@@ -1,22 +1,14 @@
 import { v1 } from '@docker/extension-api-client-types';
-import LockReset from '@mui/icons-material/LockReset';
-import Save from '@mui/icons-material/Save';
 import {
   Card,
   CardActionArea,
   CardContent,
   CircularProgress,
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  IconButton,
   Stack,
-  TextField,
   Typography,
 } from '@mui/material';
 import { useState } from 'react';
 
-import { MCP_POLICY_NAME } from '../../Constants';
 import { useCatalogOperations } from '../../queries/useCatalog';
 import { useSecrets } from '../../queries/useSecrets';
 import { CatalogItemRichened } from '../../types/catalog';
@@ -32,15 +24,9 @@ type TileProps = {
 };
 
 const Tile = ({ item, client, registryLoading }: TileProps) => {
-  const [showSecretDialog, setShowSecretDialog] = useState(false);
   const [assignedSecrets] = useState<{ name: string; assigned: boolean }[]>([]);
-  const [changedSecrets, setChangedSecrets] = useState<{
-    [key: string]: string | undefined;
-  }>({});
-  const [secretLoading, setSecretLoading] = useState(false);
   const [showConfigModal, setShowConfigModal] = useState(false);
-  const { isLoading: secretsLoading, mutate: mutateSecret } =
-    useSecrets(client);
+  const { isLoading: secretsLoading } = useSecrets(client);
   const { registerCatalogItem, unregisterCatalogItem } =
     useCatalogOperations(client);
 
@@ -57,88 +43,6 @@ const Tile = ({ item, client, registryLoading }: TileProps) => {
 
   return (
     <>
-      <Dialog
-        open={showSecretDialog}
-        onClose={() => setShowSecretDialog(false)}
-      >
-        <DialogTitle>
-          <Typography variant="h6">Secrets</Typography>
-        </DialogTitle>
-        <DialogContent>
-          <Stack direction="column" spacing={2}>
-            {assignedSecrets?.map((secret) => {
-              const isAssigned = assignedSecrets.find(
-                (s) => s.name === secret.name
-              );
-              return (
-                <Stack
-                  key={secret.name}
-                  direction="row"
-                  spacing={2}
-                  alignItems="center"
-                >
-                  <Typography variant="body2">
-                    {secret.name}{' '}
-                    {isAssigned?.assigned ? 'assigned' : 'not assigned'}
-                  </Typography>
-                  <TextField
-                    placeholder={
-                      isAssigned?.assigned ? '********' : 'Enter secret value'
-                    }
-                    type="password"
-                    key={secret.name}
-                    label={secret.name}
-                    value={changedSecrets[secret.name] || ''}
-                    onChange={(event) =>
-                      setChangedSecrets({
-                        ...changedSecrets,
-                        [secret.name]: event.target.value,
-                      })
-                    }
-                  />
-                  {isAssigned?.assigned && changedSecrets[secret.name] && (
-                    <IconButton
-                      onClick={() =>
-                        setChangedSecrets({
-                          ...changedSecrets,
-                          [secret.name]: undefined,
-                        })
-                      }
-                    >
-                      <LockReset />
-                    </IconButton>
-                  )}
-                  {changedSecrets[secret.name] && (
-                    <IconButton
-                      onClick={() => {
-                        setSecretLoading(true);
-                        mutateSecret
-                          .mutateAsync({
-                            name: secret.name,
-                            value: changedSecrets[secret.name] || '',
-                            policies: [MCP_POLICY_NAME],
-                          })
-                          .then(() => {
-                            setSecretLoading(false);
-                            const newChangedSecrets = { ...changedSecrets };
-                            delete newChangedSecrets[secret.name];
-                            setChangedSecrets(newChangedSecrets);
-                          });
-                      }}
-                    >
-                      {secretLoading ? (
-                        <CircularProgress size={20} />
-                      ) : (
-                        <Save />
-                      )}
-                    </IconButton>
-                  )}
-                </Stack>
-              );
-            })}
-          </Stack>
-        </DialogContent>
-      </Dialog>
       {showConfigModal && (
         <ConfigurationModal
           open={showConfigModal}
