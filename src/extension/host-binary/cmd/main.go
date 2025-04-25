3827ce271aa0b75ae1094cbd86de2066e05d3d2d
Johannes Großmann <grossmann.johannes@t-online.de>
2025-04-25 10:02:35 +0200

    feat(oauth): add commands to list and authorize oauth apps

--- src/extension/host-binary/cmd/main.go
+++ src/extension/host-binary/cmd/main.go
@@ -20,6 +20,9 @@ func main() {
 	cmd := AddSecret(ctx)
 	cmd.AddCommand(ListSecrets(ctx))
 	cmd.AddCommand(DeleteSecret(ctx))
+	cmd.AddCommand(AuthorizeApp(ctx))
+	cmd.AddCommand(UnauthorizeApp(ctx))
+	cmd.AddCommand(ListOAuthApps(ctx))
 	if err := cmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -51,6 +54,97 @@ func newOAuthApiClient() (client.OAuthApiClient, error) {
 		return nil, err
 	}
 	return client.NewOAuthApiClient(p), nil
+}
+
+func ListOAuthApps(ctx context.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list-oauth-apps",
+		Short: "List all OAuth apps",
+		Args:  cobra.NoArgs,
+		RunE: func(*cobra.Command, []string) error {
+			return runListOAuthApps(ctx)
+		},
+	}
+	return cmd
+}
+
+func runListOAuthApps(ctx context.Context) error {
+	c, err := newOAuthApiClient()
+	if err != nil {
+		return err
+	}
+	apps, err := c.ListOAuthApps(ctx)
+	if err != nil {
+		return err
+	}
+	return json.NewEncoder(os.Stdout).Encode(apps)
+}
+
+type authorizeOptions struct {
+	Name   string
+	Scopes string
+}
+
+func AuthorizeApp(ctx context.Context) *cobra.Command {
+	opts := &authorizeOptions{}
+	cmd := &cobra.Command{
+		Use:   "authorize",
+		Short: "Authorize an OAuth app",
+		Args:  cobra.NoArgs,
+		RunE: func(*cobra.Command, []string) error {
+			return runAuthorizeApp(ctx, *opts)
+		},
+	}
+	flags := cmd.Flags()
+	flags.StringVarP(&opts.Name, "name", "n", "", "Name of the OAuth app")
+	_ = cmd.MarkFlagRequired("name")
+	flags.StringVarP(&opts.Scopes, "scopes", "s", "", "Scopes for the OAuth app")
+	return cmd
+}
+
+func runAuthorizeApp(ctx context.Context, opts authorizeOptions) error {
+	c, err := newOAuthApiClient()
+	if err != nil {
+		return err
+	}
+	authResponse, err := c.PostOAuthApp(ctx, opts.Name, opts.Scopes)
+	if err != nil {
+		return err
+	}
+	return json.NewEncoder(os.Stdout).Encode(authResponse)
+}
+
+type unauthorizeOptions struct {
+	Name string
+}
+
+func UnauthorizeApp(ctx context.Context) *cobra.Command {
+	opts := &unauthorizeOptions{}
+	cmd := &cobra.Command{
+		Use:   "unauthorize",
+		Short: "Unauthorize an OAuth app",
+		Args:  cobra.NoArgs,
+		RunE: func(*cobra.Command, []string) error {
+			return runUnauthorizeApp(ctx, *opts)
+		},
+	}
+	flags := cmd.Flags()
+	flags.StringVarP(&opts.Name, "name", "n", "", "Name of the OAuth app")
+	_ = cmd.MarkFlagRequired("name")
+	return cmd
+}
+
+func runUnauthorizeApp(ctx context.Context, opts unauthorizeOptions) error {
+	c, err := newOAuthApiClient()
+	if err != nil {
+		return err
+	}
+	err = c.DeleteOAuthApp(ctx, opts.Name)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("App %s has been unauthorised\n", opts.Name)
+	return nil
 }
 
 type addOptions struct {
