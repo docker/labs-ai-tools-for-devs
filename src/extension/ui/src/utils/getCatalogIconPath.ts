034de2a0138ba61a4f8af70bddc1ab120b60faa4
Trung Nguyen <trung.nguyen@docker.com>
2025-04-30 17:12:34 +0200

    Avoid reloading icons
    Signed-off-by: Trung Nguyen <trung.nguyen@docker.com>

--- src/extension/ui/src/utils/getCatalogIconPath.ts
+++ src/extension/ui/src/utils/getCatalogIconPath.ts
@@ -1,34 +1,43 @@
-import { md5 } from "js-md5";
+import { md5 } from 'js-md5';
+
+const iconsCache = {} as Record<string, string>;
 
 const getCatalogIconPath = async (iconUrl: string) => {
-    try {
-        var hash = md5.hex(iconUrl);
-        const hrefIcon = new URL(import.meta.url + `/../../static-assets/${hash}`).href
+  if (iconsCache[iconUrl]) {
+    return iconsCache[iconUrl];
+  }
+  try {
+    var hash = md5.hex(iconUrl);
+    const hrefIcon = new URL(import.meta.url + `/../../static-assets/${hash}`)
+      .href;
 
-        await checkIfImageExists(hrefIcon);
-        return hrefIcon
-    } catch {
-        return iconUrl
-    }
-}
+    await checkIfImageExists(hrefIcon);
+    iconsCache[iconUrl] = hrefIcon;
+
+    return hrefIcon;
+  } catch {
+    iconsCache[iconUrl] = iconUrl;
+    return iconUrl;
+  }
+};
 
 function checkIfImageExists(url: string) {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.src = url;
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = url;
 
-        if (img.complete) {
-            resolve(true)
-        } else {
-            img.onload = () => {
-                resolve(true)
-            };
+    if (img.complete) {
+      resolve(true);
+    } else {
+      img.onload = () => {
+        resolve(true);
+      };
 
-            img.onerror = () => {
-                reject(false)
-            };
-        }
-    });
+      img.onerror = () => {
+        reject(false);
+      };
+    }
+  });
 }
 
 export default getCatalogIconPath;
