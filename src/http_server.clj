945906086b66d44fbdb92305d9a8201f28534117
Jim Clark <slimslenderslacks@gmail.com>
2025-05-07 22:06:36 -0700

    Update tools endpoint to start socket

--- src/http_server.clj
+++ src/http_server.clj
@@ -1,37 +1,45 @@
 (ns http-server
-  (:require [ring.adapter.jetty :as jetty]
-            [compojure.core :refer [defroutes POST GET]]
-            [ring.util.response :refer [response]]
-            [compojure.route :as route]
-            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
-            [babashka.curl :as curl]
-            ))
+  (:require
+   [babashka.curl :as curl]
+   [compojure.core :refer [GET POST routes]]
+   [compojure.route :as route]
+   [jsonrpc.logger :as logger]
+   [jsonrpc.server]
+   [ring.adapter.jetty :as jetty]
+   [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+   [ring.util.response :refer [response]]))
 
-(defroutes app-routes
+(def next-port (atom 8811))
+
+(defn app-routes [server-opts]
   ;; POST route for /tools that accepts JSON
-  (POST "/tools" request
-    (let [body (:body request)]
-      (println "Received JSON body:" body)
-      (response {:message "Received your request" 
-                 :received body})))
-  
-  ;; Default route for root path
-  (GET "/" []
-    {:status 200
-     :headers {"Content-Type" "text/html"}
-     :body "Hello, World! This is a simple Jetty server in Clojure."})
-  
-  ;; Handle 404 Not Found
-  (route/not-found "Not Found"))
+  (routes
+    (POST "/tools" request
+          (let [body (:body request)
+                port (swap! next-port inc) ]
+            (logger/info "Received JSON body:" body)
+            (logger/info "Create new endpoint on port: " port)
+            (jsonrpc.server/run-socket-server! {:port port} server-opts)
+            (response {:port port
+                       :host "host.docker.internal"})))
 
-(def app
-  (-> app-routes
+    ;; Default route for root path
+    (GET "/" []
+         {:status 200
+          :headers {"Content-Type" "text/html"}
+          :body "Hello, World! This is a simple Jetty server in Clojure."})
+
+    ;; Handle 404 Not Found
+    (route/not-found "Not Found")))
+
+(defn app [server-opts]
+  (-> (app-routes server-opts)
       wrap-json-response
       (wrap-json-body {:keywords? true})))
 
-(defn start []
-  (println "Server starting on port 3000")
-  (jetty/run-jetty app {:port 3000 :join? true}))
+(defn start [server-opts]
+  (logger/info "Server starting on port 3000")
+  (jetty/run-jetty (app server-opts) {:port 3000 :join? true}))
 
 (comment
   (require '[cheshire.core :as json])
