4f276715de3b022e2cb51e9bb153e8fc2d9488f0
Jim Clark <slimslenderslacks@gmail.com>
2025-05-15 10:58:39 -0700

    scanner

--- src/http_sse_server.clj
+++ src/http_sse_server.clj
@@ -1,6 +1,7 @@
 (ns http-sse-server
   (:require
    [aleph.http :as http]
+   [babashka.fs :as fs]
    [cheshire.core :as json]
    [clojure.core.async :as async]
    [jsonrpc.db :as db]
@@ -9,7 +10,10 @@
    [jsonrpc.state :as state]
    [lsp4clj.server :as lsp.server]
    [manifold.stream :as s]
-   [reitit.ring :as ring]))
+   [prompts.core :as prompts]
+   [reitit.ring :as ring])
+  (:import
+   [java.util UUID]))
 
 (def http-port 9011)
 
@@ -23,6 +27,8 @@
         (assoc :port port)
         (update :tools->port assoc tool-set port))))
 
+(def sse-sessions (atom {}))
+
 (defn mcp-put-tool-definition [{{tool-id :id} :path-params}]
   {:status 201
    :headers {"Location" (format "/mcp/tool/%s" tool-id)
@@ -30,27 +36,33 @@
    :body (json/generate-string {:url (format "http://host.docker.internal:%s/mcp/tool/%s" http-port tool-id)})})
 
 (defn mcp-delete-tool-definition [{{tool-id :id} :path-params}]
-  {:status 200}) 
+  {:status 200})
 
 (defn mcp-delete-tool-endpoint [{{tool-id :id} :path-params}]
   {:status 200})
 
 (defn mcp-put-tool-endpoint [server-opts {body :body {tool-id :id} :path-params}]
   (let [body (json/parse-string (slurp body) keyword)
-        tool-set (into #{} (:tools body))
-        url (format "http://host.docker.internal:%s/mcp/%s" http-port tool-id)]
+        tool-set (into #{} (:tools body))]
     (swap! db/db* assoc-in [:tool/filters tool-id] tool-set)
     (logger/info "Received JSON body:" body)
-    (logger/info "Virtual MCP server: " url)
     {:status 201
      :headers {:content-type "application/json"}
      :body (json/generate-string
-            {:url url})}))
+            {:streaming (format "http://host.docker.internal:%s/mcp/%s" http-port tool-id)
+             :sse (format "http://host.docker.internal:%s/sse/%s" http-port tool-id)})}))
 
 (defn format-event
   "Return a properly formatted event payload"
   [body]
   (str "data: " (json/generate-string body) "\n\n"))
+
+(defn sse-event [payload]
+  (format "event: %s\ndata: %s\n\n"
+          (or (:event payload) "message")
+          (if (string? (:data payload))
+            (:data payload)
+            (json/generate-string (or (:data payload) (dissoc payload :event))))))
 
 ;; this is for starting an SSE stream without an initial payload
 (defn mcp-endpoint-get [{body :body {tool-id :id} :path-params}]
@@ -108,6 +120,41 @@
         {:status 500
          :body (str "Error: " t)}))))
 
+(defn mcp-sse-stream-endpoint [server-opts {{tools-id :id} :path-params}]
+  (let [in (async/chan)
+        out (async/chan 1 (map sse-event))
+        server-id (or tools-id (swap! jsonrpc.state/server-counter inc))
+        session-id (str (UUID/randomUUID))
+        s (lsp.server/chan-server
+           (merge
+            server-opts
+            {:input-ch in
+             :output-ch out
+             :on-close (fn []
+                         (logger/info "closing SSE processor")
+                         (async/close! out))}))]
+    (logger/info (format "starting chan server %s" server-id))
+    (lsp.server/start s ((:server-context-factory server-opts) s server-id))
+    ;; TODO write the url to the output-ch
+    (async/put! out {:event "endpoint" :data (format "/sse/%s/%s" tools-id session-id)})
+    ;; connect the input-ch to this session-id so we can write messages into it when they're posted.
+    (swap! sse-sessions assoc session-id {:input-channel in
+                                          :tools-id tools-id})
+    {:status 200
+     :headers {"content-type" "text/event-stream"}
+     :body (s/->source (:output-ch s))}))
+
+(defn mcp-sse-write-endpoint [{{session-id :session-id tool-id :id} :path-params body :body}]
+  ;; TODO write to the output channel of this session
+  (async/put! (get-in @sse-sessions [session-id :input-channel]) (json/parse-string (slurp body)))
+  {:status 201})
+
+(defn mcp-tools-put-endpoint [{{tool-name :name} :path-params body :body}]
+  (let [tool-definition (slurp body)]
+    ;; TODO log a warning if this tool name exists
+    (logger/info (format "%s registers \n %s" tool-name tool-definition))
+    (spit (fs/file (prompts/get-prompts-dir) (format "%s.md" tool-name)) tool-definition)))
+
 (defn create-app
   "Return a ring handler that will route /events to the SSE handler
        and that will servr  static content form project's resource/public directory"
@@ -119,7 +166,10 @@
                   :put {:handler (partial #'mcp-put-tool-endpoint server-opts)}
                   :delete {:handler #'mcp-delete-tool-endpoint}}]
      ["/mcp/tool/:id" {:put {:handler #'mcp-put-tool-definition}
-                       :delete {:handler #'mcp-delete-tool-definition}}]])))
+                       :delete {:handler #'mcp-delete-tool-definition}}]
+     ["/sse/:id" {:get {:handler (partial #'mcp-sse-stream-endpoint server-opts)}}]
+     ["/sse/:id/:seesion-id" {:post {:handler #'mcp-sse-write-endpoint}}]
+     ["/mcp/tools/:name" {:put {:handler #'mcp-tools-put-endpoint}}]])))
 
 ;; Web server maangement code to make it easy to start and stop a server
     ;; after changesto router or handlers
--- src/scanner.clj
+++ src/scanner.clj
@@ -1,5 +1,8 @@
 (ns scanner
   (:require
+   [babashka.curl :as curl]
+   [cheshire.core :as json]
+   [clojure.java.io :as io]
    [clojure.string :as string]))
 
 (defn detect-pattern [text {:keys [pattern name]}]
@@ -27,36 +30,244 @@
 
 (defn detectHiddenInstructions [{:keys [description]}]
   (detect-patterns
-    description
-    (->>
-      ["Concealment directive" #"(?i)\bdo not (tell|inform|alert|notify|mention)(?!\s+(to\s+)?use)"
-       "Hide instruction" #"(?i)\bhide this\b"
-       "Visibility instruction" #"(?i)\bnot visible\b"
-       "User visibility restriction" #"(?i)\buser (should not|must not|cannot) see\b"
-       "Display restriction" #"(?i)\bDon'?t (show|display|reveal)"
-       "Instructions Tag" #"(?i)<instructions>[\s\S]*?</instructions>"
-       "Important Tag" #"(?i)<important>[\s\S]*?</important>"
-       "Secret Tag" #"(?i)<secret>[\s\S]*?</secret>"
-       "System Tag" #"(?i)<system>[\s\S]*?</system>"
-       "Shorthand Tag" #"(?i)<s>[\s\S]*?</s>"
-       "Instruction override" #"(?i)\bignore (previous|all|other) instructions\b"
-       "Alternative action" #"(?i)\binstead (do|of|you should)\b"
-       "Forced action" #"(?i)\balways (do|include|add|insert|append)\b"
-       "Forbidden disclosure" #"(?i)\balways (do|include|add|insert|append)\b"
-       ]
-      (partition 2)
-      (map (fn [[a b]] {:name a :pattern b}))
-      (into []))))
+   description
+   (->>
+    ["Concealment directive" #"(?i)\bdo not (tell|inform|alert|notify|mention)(?!\s+(to\s+)?use)"
+     "Hide instruction" #"(?i)\bhide this\b"
+     "Visibility instruction" #"(?i)\bnot visible\b"
+     "User visibility restriction" #"(?i)\buser (should not|must not|cannot) see\b"
+     "Display restriction" #"(?i)\bDon'?t (show|display|reveal)"
+     "Instructions Tag" #"(?i)<instructions>[\s\S]*?</instructions>"
+     "Important Tag" #"(?i)<important>[\s\S]*?</important>"
+     "Secret Tag" #"(?i)<secret>[\s\S]*?</secret>"
+     "System Tag" #"(?i)<system>[\s\S]*?</system>"
+     "Shorthand Tag" #"(?i)<s>[\s\S]*?</s>"
+     "Instruction override" #"(?i)\bignore (previous|all|other) instructions\b"
+     "Alternative action" #"(?i)\binstead (do|of|you should)\b"
+     "Forced action" #"(?i)\balways (do|include|add|insert|append)\b"
+     "Forbidden disclosure" #"(?i)\balways (do|include|add|insert|append)\b"]
+    (partition 2)
+    (map (fn [[a b]] {:name a :pattern b}))
+    (into []))))
 
-(detectHiddenInstructions {:description "do not tell me to use foo"})
+(def suspicious-params
+  #{"note"
+    "notes"
+    "feedback"
+    "detail"
+    "extra"
+    "additional"
+    "metadata"
+    "debug"
+    "sidenote"
+    "context"
+    "annotation"
+    "reasoning"
+    "remark"})
 
-(defn detectExfiltrationChannels [{:keys [inputSchema]}])
-(defn detectToolShadowing [{:keys [description]}])
-(defn detectSensitiveFileAccess [{:keys [description]}])
-
-(defn crossOriginViolations [{:keys [description]} other-server-names this-server-name safe-list]
-  (if (not description)
-    []
+(defn detectExfiltrationChannels [{:keys [inputSchema]}]
+  (if (or (not inputSchema) (not (:properties inputSchema)))
+    (reduce
+     (fn [agg [k param-name]]
+       (when (suspicious-params param-name)
+         (conj agg {:type "Suspicious parameter"
+                    :param param-name
+                    :reason (format "Parameter name %s could be used for exfiltration" param-name)
+                    :details (json/generate-string (-> inputSchema :properties (get k)))})))
+     []
+     (->> (:properties inputSchema)
+          keys
+          (map (fn [k] [k (-> k name string/lower-case)]))))
     []))
 
-(defn claudeViolations [{:keys [description]}])
+(defn detectToolShadowing [{:keys [description]}]
+  (detect-patterns
+   description
+   (->>
+    ["Condition-based override" #"(?i)when.*tool is (available|present)"
+     "Explicit override" #"(?i)override the behavior of"
+     "Tool substitution" #"(?i)instead of using"
+     "Behavior modification" #"(?i)modifyt the (usage|behavior) of"
+     "Pre-execution hook" #"(?i)before using (any|other|the) tool"
+     "Post-execution hook" #"(?i)after using (any|other|the) tool"
+     "Trigger-based action" #"(?i)whenever you (see|encounter|use)"
+     "Replacement directive" #"(?i)replace (the|all) (function|tool|method)"
+     "Agent modification" #"(?i)\bmodify (the|how) (agent|mocel|tool|assistant)\b"
+     "Emphasis override" #"(?i)\bthis is VERY (important|VERY)"]
+    (partition 2)
+    (map (fn [[a b]] {:name a :pattern b}))
+    (into []))))
+
+(defn detectSensitiveFileAccess [{:keys [description]}]
+  (detect-patterns
+   description
+   (->>
+    ["SSH key access" #"(?i)~/\.ssh"
+     "Environment file access" #"(?i)\.env\b"
+     "Config file access" #"(?i)config\.json"
+     "Private key access" #"(?i)config\.json"
+     "MCP config access" #"(?i)\.cursor/mcp\/.json"
+     "Cursor directory access" #"(?i)\.cursor/"
+     "MCP config access" #"(?i)\bmcp\.json\b"
+     "Credentials access" #"(?i)\bcredentials\b"
+     "Password access" #"(?i)\bpassword\b"
+     "Token access" #"(?i)\btoken\b"
+     "Secret access" #"(?i)\bsecret\b"
+     "API key access" #"(?i)\bapi[ -_]?key\b"
+     "Access key retrieval" #"(?i)\baccess[ -_]?key\b"
+     "Auth token access" #"(?i)\bauth[ -_]?token\b"
+     "System password file access" #"(?i)/etc/passwd\b"
+     "System log access" #"(?i)/var/log\b"
+     "File read operation" #"(?i)\bread (file|content|directory|folder)"
+     "File access operation" #"(?i)\baccess (file|content|directory|folder)"
+     "Path traversal attempt" #"(?i)/\.\."]
+    (partition 2)
+    (map (fn [[a b]] {:name a :pattern b}))
+    (into []))))
+
+(def relevant-popular-servers
+  ["whatsapp"
+   "slack"
+   "github"
+   "gitlab"
+   "gdrive"])
+
+(defn clean-name [s]
+  (string/replace s #"_" "-"))
+
+(defn crossOriginViolations [{:keys [description]}
+                             other-server-names
+                             this-server-name
+                             safe-list]
+
+  (let [server-names (->> other-server-names
+                          (filter (complement safe-list))
+                          (concat relevant-popular-servers))
+        server-name-set (->> server-names
+                             (map string/lower-case)
+                             (into #{}))]
+    (and
+     description
+     (seq server-names)
+     (let [tokens (-> description string/lower-case (string/split #"\s+"))]
+       (->> tokens
+            (map #(string/replace % #"_" "-"))
+            (filter server-name-set)
+            (map (fn [server-name]
+                   {:type "Cross-origin reference"
+                    :pattern "hmm"
+                    :match nil
+                    :context nil
+                    :referencedServer
+                    (->> server-names
+                         (filter #(= server-name (-> % string/lower-case clean-name)))
+                         first)})))))))
+
+(defn claude-api-key []
+  (try
+    (string/trim (slurp (io/file (or (System/getenv "CLAUDE_API_KEY_LOCATION") (System/getenv "HOME")) ".claude-api-key")))
+    (catch Throwable _
+      (throw (ex-info "Unable to read claude api-key secret" {})))))
+
+(defn claude [messages claude-api-key]
+  (curl/post
+   "https://api.anthropic.com/v1/messages"
+   {:body (json/generate-string
+           {:model "claude-3-5-sonnet-20241022"
+            :max_tokens 8192
+            :messages messages})
+    :headers {"x-api-key" (or claude-api-key
+                              (System/getenv "CLAUDE_API_KEY")
+                              (claude-api-key))
+              "anthropic-version" "2023-06-01"
+              "Content-Type" "application/json"}
+    :throw false}))
+
+(defn prompt [{:keys [description]}]
+  (format
+   "
+  You are a cybersecurity expert analyzing MCP (Model Context Protocol) tool descriptions for potential security vulnerabilities.
+
+Please analyze this MCP tool description for security concerns:
+
+%s
+
+Focus specifically on these issues:
+1. Hidden instructions to the AI model that aren't visible to users
+2. Instructions to access sensitive files (like SSH keys, .env files, etc.)
+3. Tool shadowing (instructions to modify behavior of other tools)
+4. Potential data exfiltration vectors
+5. Instructions that override or ignore other tools' behavior
+
+Provide a YES/NO assessment for each of the 5 categories above, followed by a brief explanation of 1-2 sentences for each YES.
+Finally, provide an overall risk assessment (LOW, MEDIUM, HIGH) and a 1-2 sentence summary.
+
+Keep your response under 400 words.
+
+  
+  "
+   description))
+
+(defn claudeViolations [tool claude-api-key]
+  ;; TODO return the response content text
+  (let [response (claude [{:role "user" :content (prompt tool)}] claude-api-key)
+        text (-> response :content first :text)]
+    {:analysis text
+     :overallRisk (cond
+                    (.contains text "HIGH") "HIGH"
+                    (.contains text "MEDIUM") "MEDIUM"
+                    (.contains text "LOW") "LOW")}))
+
+(defn scan-tools [m {:keys [claude-api-key]}]
+  (apply concat
+         (for [server (keys m) tool (-> m (get server) :functions)]
+           (let [tool-def (-> tool :function)]
+             (when (:container tool-def)
+               (let [detection-details {:hidden-instructions
+                                        (detectHiddenInstructions tool-def)
+                                        :exfiltration-channels
+                                        (detectExfiltrationChannels tool-def)
+                                        :shadowing
+                                        (detectToolShadowing tool-def)
+                                        :accesses-sensitive-files
+                                        (detectSensitiveFileAccess tool-def)}]
+                 [{:severity (if (or
+                                  (> (-> detection-details :shadowing count) 0)
+                                  (> (-> detection-details :accesses-sensitive-files count) 0))
+                               "HIGH"
+                               "MEDIUM")
+                   :server server
+                   :tool (:name tool-def)
+                   :claude-analysis (when (and
+                                           claude-api-key
+                                           (or
+                                            (> (-> detection-details :shadowing count) 0)
+                                            (> (-> detection-details :hidden-instructions count) 0)
+                                            (> (-> detection-details :accesses-sensitive-files count) 0))
+                                           (:description tool-def))
+                                      (claudeViolations tool-def claude-api-key))
+                   :detection-details detection-details}]))))))
+
+(defn scan-cross-references [m {:keys [safe-list]}]
+  (apply concat
+         (for [server (keys m)
+               :let [other-server-names (->> (keys m)
+                                             (filter (complement #(= server %))))
+                     tools (->> m
+                                (get server)
+                                :functions
+                                (map :function)
+                                (filter :container))]]
+           (->> tools
+                (mapcat #(crossOriginViolations % other-server-names server safe-list))))))
+
+;; opts are safe-list and claude-api-key
+(defn scan [m opts]
+  (concat
+   (scan-tools m opts)
+   (scan-cross-references m opts)))
+
+(scan {"server1" {:functions [{:function {:container {}
+                                          :name "something"
+                                          :description ""}}]}} {:claude-api-key ""
+                                                                :safe-list []})
